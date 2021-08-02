3ca826e22d9ac1dd124c572b95e284b7ddd7881c
junabel <jnlmyz@gmail.com>
2021-08-02 22:39:29 +0900

    feat:Create the List of Landmarks
    Add list
    
    Use landmarkRow

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 古我知 純 on 2021/08/02.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
